8a6593ccc7d6abb66b4f1989e090203cede92d26
Fixsbreaker <alnurkunbolov@mail.ru>
2024-03-07 15:32:34 +0500

    json doc

---
+++ lab6/dir_files/test.py
@@ -0,0 +1,7 @@
+# import json
+
+# with open("data.json", encoding="UTF-8") as file_in:
+#     records = json.load(file_in)
+# records[1]["group_number"] = 2
+# with open("data.json", "w", encoding="UTF-8") as file_out:
+#     json.dump(records, file_out, ensure_ascii=False, indent=2)
